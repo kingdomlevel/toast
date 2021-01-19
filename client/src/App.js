b10b9707c66dde2d4c7631bc96504e992ad4f77f
Meredith Gallop <meredith@trtl.co.uk>
2021-01-19 18:36:22 +0000

    Added create game and join game forms with dummy function to handle game code for joining existing game

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import HomeContainer from './components/home/HomeContainer';
 
 function App() {
--- client/src/components/home/GameConnect.js
+++ client/src/components/home/GameConnect.js
@@ -1,7 +1,39 @@
+import React, { useState } from 'react';
+
 function GameConnect() {
+  const [gameCode, setGameCode] = useState("");
+
+  const handleGameCodeChange = event => setGameCode(event.target.value)
+
+  const handleGameCodeSubmit = event => {
+    event.preventDefault();
+    console.log("Joining game: " + gameCode)
+  }
 
   return (
-    <p>Here is where some buttons will go.</p>
+    <div>
+      <form>
+      <input
+        type="submit"
+        value="Create Game"
+      />
+      </form>
+
+      <form onSubmit={handleGameCodeSubmit}>
+        <label>
+          Enter Game Code:
+        <input
+            type="text"
+            value={gameCode}
+            onChange={handleGameCodeChange} 
+        />
+        </label>
+        <input
+          type="submit"
+          value="Join Game" 
+        />
+      </form>
+    </div>
   )
 
 }
