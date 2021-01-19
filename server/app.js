1af93e3def612cebe93752224392cec63dc00b92
kingdomlevel <nllmrrs@aol.com>
2021-01-19 16:49:53 +0000

    socket.io ready to accept connections on http server instance

--- server/app.js
+++ server/app.js
@@ -1,6 +1,20 @@
 const express = require('express');
 const app = express();
 const http = require('http').Server(app);
+const io = require("socket.io")(http, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"]
+  }
+});
+
+// socket.io connections
+io.on('connection', socket => {
+  // ready 2 listen for things here!
+  // socket is key.... 
+  //     - id at `socket.id`
+  //     - listen for stuff `socket.on('event-name', callback);`
+});
 
 http.listen(5000, function() {
   console.log(`app is listening on port ${this.address().port} `);
