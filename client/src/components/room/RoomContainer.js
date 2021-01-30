7cb242e92077520fd11a8997c582baac438a3dd7
kingdomlevel <nllmrrs@aol.com>
2021-01-30 16:56:27 +0000

    #11 adds ranndom nickname generator

--- client/src/components/room/RoomContainer.js
+++ client/src/components/room/RoomContainer.js
@@ -1,9 +1,14 @@
+import { useState } from 'react';
+import {generateNickname} from '../../utils/randomWords';
+
 const RoomContainer = ({roomCode, isHost}) => {
+  const [nickname] = useState(generateNickname());
   const hostIntro = isHost ? 'i see you are the host!' : '';
 
   return (
     <>
       <p>{hostIntro}</p>
+      <p>Hello {nickname}!</p>
       <p>Welcome to the room of {roomCode}!</p>
     </>
   )
--- client/src/utils/randomWords.js
+++ client/src/utils/randomWords.js
@@ -1,4 +1,5 @@
 const randomWords = require('random-words');
+const { uniqueNamesGenerator, adjectives, names } = require('unique-names-generator');
 
 export const generateGameCode = () => {
   const [newGameCode] = randomWords({
@@ -9,3 +10,14 @@ export const generateGameCode = () => {
 
   return newGameCode;
 }
+
+export const generateNickname = () => {
+  const nickname = uniqueNamesGenerator({
+    dictionaries: [adjectives, names],
+    length: 2,
+    separator: '',
+    style: 'capital'
+  });
+
+  return nickname;
+};
