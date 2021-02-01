b8e7ba66746d5b13b09cfbc3c150cd3f388b08ce
kingdomlevel <nllmrrs@aol.com>
2021-02-01 18:36:29 +0000

    #11 server socket.io accepting connections from clients and adding to allRooms var

--- server/models/Player.js
+++ server/models/Player.js
@@ -3,13 +3,6 @@ class Player {
     this.socketId = socketId;
     this.nickname = nickname;
   }
-
-    toString() {
-      `{
-        socketId: ${this.socketId},
-        nickname: ${this.nickname}
-      }`
-    }
 }
 
 module.exports = Player
